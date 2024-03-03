2e4bb7e309ed8b8bdd813410b3659bdaff45624a
KennBro <kennbro@protonmail.com>
2024-03-03 20:43:53 -0300

    [INI] Initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need
+# fine tuning.
+build_options = {'packages': [], 'excludes': []}
+
+base = 'console'
+
+executables = [
+    Executable('app.py', base=base, target_name = 'trackenn')
+]
+
+setup(name='trackenn',
+      version = '0.1',
+      description = 'Blockchain traceability',
+      options = {'build_exe': build_options},
+      executables = executables)
