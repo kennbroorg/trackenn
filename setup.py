0ae31e7ddc25a5769260df5e576ad88442033e0e
Kenn <kennbro@protonmail.com>
2024-05-08 14:29:58 -0300

    [ADD] Core

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need
 # fine tuning.
-build_options = {'packages': [], 'excludes': []}
+build_options = {'packages': ['core'], 'excludes': []}
 
 base = 'console'
 
